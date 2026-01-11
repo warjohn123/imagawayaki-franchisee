9e4d7e362463b6aa8bf251286dcbc2ca8b3aafee
Warren John Caruana <warrencaruana1@gmail.com>
2026-01-11 08:34:57 +0800

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
